4d89e059b5467006b22a68e51a315f30ddf645c7
asfamilybank <tanggangchao@outlook.com>
2022-07-19 15:26:42 +0800

    fix(icons): fix icon center bug

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -2,5 +2,11 @@ import { SvgIcon, SvgIconProps } from '@mui/material'
 import ArrowUpSvg from 'assets/svg/arrow-up.svg'
 
 export const ArrowUpIcon = (props: SvgIconProps) => {
-  return <SvgIcon component={ArrowUpSvg as React.ElementType} {...props} />
+  return (
+    <SvgIcon
+      component={ArrowUpSvg as React.ElementType}
+      viewBox="0 0 32 32"
+      {...props}
+    />
+  )
 }
