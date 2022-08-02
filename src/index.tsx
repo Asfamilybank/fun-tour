a01a79d5d43ea08364a9f30012bc71e71cba2165
asfamilybank <tanggangchao@outlook.com>
2022-08-02 17:02:31 +0800

    feat: add react-hook-form

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,11 @@ import { AliveScope } from 'react-activation'
 import { createRoot } from 'react-dom/client'
 
 import { BrowserRouter } from 'react-router-dom'
-import { RecoilRoot, useRecoilValue } from 'recoil'
+import { RecoilRoot } from 'recoil'
 import Router from 'router'
-import { darkMode } from 'store/app'
 import 'styles/index.css'
 
 const App = () => {
-  const mode = useRecoilValue(darkMode)
-
   return (
     <BrowserRouter>
       <AliveScope>
