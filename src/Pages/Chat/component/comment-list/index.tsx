b81e2cf05eff61c7b5fa57adbd75eed7542f297e
asfamilybank <tanggangchao@outlook.com>
2022-11-09 16:27:31 +0800

    fix

--- src/Pages/Chat/component/comment-list/index.tsx
+++ src/Pages/Chat/component/comment-list/index.tsx
@@ -1,12 +1,22 @@
+import { friendApi } from 'Api'
+import { Outlet, useParams } from 'react-router-dom'
+import { useData } from 'Utils/hooks'
+
 const CommentListItem = () => {
   return <div></div>
 }
 
 const CommentList = () => {
+  const { id } = useParams()
+
+  console.log(id)
+
   return (
     <div className="flex h-full w-full">
       <div></div>
-      <div></div>
+      <div>
+        <Outlet />
+      </div>
     </div>
   )
 }
